e0304f34291b4a3268a0796a1c972cbecec3ae51
danlproberts <danlproberts@gmail.com>
2019-03-12 17:31:38 +0000

    Updates

---
+++ precache-manifest.a3cd39f86c5710c78ed731b04f6a50a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sentiment-analysis-tool/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aebc78d6ef65f344429f",
+    "url": "/sentiment-analysis-tool/static/js/runtime~main.59e201d7.js"
+  },
+  {
+    "revision": "d53f47d4bb711de7df9b",
+    "url": "/sentiment-analysis-tool/static/js/main.02e6034a.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/sentiment-analysis-tool/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "d53f47d4bb711de7df9b",
+    "url": "/sentiment-analysis-tool/static/css/main.5741aaa7.chunk.css"
+  },
+  {
+    "revision": "4ba4c80d3685580b5db323739ea384a1",
+    "url": "/sentiment-analysis-tool/index.html"
+  }
+];
